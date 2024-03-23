e5e6d2cd5060fb0fbf8478c28c41a83a43b58570
Matteo Vincenti <6958534+MatteoVincenti@users.noreply.github.com>
2024-03-23 18:04:40 +0100

    feat: enhances flexibility, reliability, and maintainability of the monitoring stack, integrate Loki and Alloy services, update Prometheus and InfluxDB configurations
        - Added Loki and Alloy services to the Docker setup for enhanced monitoring.
        - Updated `.env.example` and `docker-compose.yaml` for improved security and configuration.
        - Corrected and cleaned up Prometheus and InfluxDB datasource configurations in `docker-compose.yaml`.
        - Removed obsolete Grafana provisioning files and updated settings for better Loki integration.
        - Updated `README` and `CHANGELOG.md` to reflect changes related to Loki and configuration adjustments.
        - Set InfluxDB datasource as non-editable for security.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to `PiMonitor` will be documented in this file.
+
+## 25.02.2024
+- Add Grafana Loki
+- Refactor settings
+
+## 24.02.2024
+- Add Prometheus
+- Some fix
+- Updated readme
---
+++ scripts/start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# Determine the script's directory
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+PROJECT_ROOT=$(realpath "$SCRIPT_DIR/..")
+
+# Function to start services with multiple compose files
+start_services() {
+  compose_files=("$@")
+  docker compose "${compose_files[@]}" up -d
+}
+
+# Main script logic
+compose_files=(-f "$PROJECT_ROOT/docker-compose.yaml")
+
+# Function to prompt user for service selection
+prompt_service() {
+  local service_name=$1
+  local compose_file=$2
+  read -p "Do you want to include $service_name? (y/n): " choice
+  if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    compose_files+=(-f "$compose_file")
+  fi
+}
+
+# Prompt user to select additional services to start
+echo "Select additional services to start:"
+prompt_service "homarr" "$PROJECT_ROOT/docker-compose.homarr.yaml"
+prompt_service "portainer" "$PROJECT_ROOT/docker-compose.portainer.yaml"
+prompt_service "watchtower" "$PROJECT_ROOT/docker-compose.watchtower.yaml"
+
+echo "Starting services with compose files: ${compose_files[*]}"
+start_services "${compose_files[@]}"
+
+echo "Initialization complete."
