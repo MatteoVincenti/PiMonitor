52db347441fdf1937ea2705b3a14f978f736cf9e
Matteo Vincenti <MatteoVincenti@users.noreply.github.com>
2025-02-15 14:09:19 +0100

    Feature add homarr (#1)
    * add initial support for Homarr and Portainer services with Docker Compose files, update example environment variables, and create changelog and license files
    
    * update CHANGELOG.md to reflect recent changes and improvements
    
    * replace 'docker-compose' with 'docker compose' in start.sh and stop.sh scripts
    
    * fix: correct container name indentation and simplify port mapping in Homarr Docker Compose file
    
    * feat: add interactive prompts for selecting additional services in start.sh
    
    * fix: update .gitignore to exclude homarr/appdata directory

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+
+All notable changes to `PiMonitor` will be documented in this file.
+
+### Added
+- Added dependency for telegraf in `docker-compose.yml`.
+- Split docker compose into multiple files to start only the wanted services, updated the `README.md`.
+- Added new docker compose files for specified optional services.
+- Added name on docker compose file.
+
+### Updated
+- Updated telegraf config and `docker-compose.yml`.
+- Updated some settings.
+- Updated telegraf config and docker-compose.
+- Edited `README.md`.
+- Updated docker compose.
+
+### Removed
+- Removed some configs from `telegraf.conf`.
+
+## [Initial Release]
+- Initial commit.
---
+++ bin/start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Function to start services with multiple compose files
+start_services() {
+  compose_files=("$@")
+  docker compose "${compose_files[@]}" up -d
+}
+
+# Main script logic
+compose_files=(-f ../docker-compose.yml)
+
+# Function to prompt user for service selection
+prompt_service() {
+  local service_name=$1
+  local compose_file=$2
+  read -p "Do you want to include $service_name? (y/n): " choice
+  if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    compose_files+=(-f "$compose_file")
+  fi
+}
+
+# Prompt user to select additional services to start
+echo "Select additional services to start:"
+prompt_service "homarr" "../docker-compose.homarr.yml"
+prompt_service "portainer" "../docker-compose.portainer.yml"
+prompt_service "watchtower" "../docker-compose.watchtower.yml"
+
+echo "Starting services with compose files: ${compose_files[*]}"
+start_services "${compose_files[@]}"
+
+echo "Initialization complete."
