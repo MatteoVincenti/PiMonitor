129b3abef08bba32735c288a798a89f3ca2e09bf
Matteo Vincenti <Vimat1990@gmail.com>
2025-02-24 18:26:00 +0100

    fix build-provisioning script: update path for provisioning files to use workspace directory

--- grafana/scripts/build-provisioning.sh
+++ grafana/scripts/build-provisioning.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Replacing environment variables in provisioning files..."
-find /provisioning/ -type f -name "*.yaml.template" | while read -r file; do
-  target_file="/provisioning/${file##*/}"
+find /workspace/provisioning -type f -name "*.yaml.template" | while read -r file; do
+  target_file="/workspace/provisioning/${file##*/}"
   target_file="${target_file%.template}"
   envsubst < "$file" > "$target_file"
   echo "Processed: $file -> $target_file"
