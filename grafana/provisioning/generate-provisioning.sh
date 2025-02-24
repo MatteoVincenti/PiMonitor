44626a294beb8badb506edd152a255fb1a439196
Matteo Vincenti <Vimat1990@gmail.com>
2025-02-24 21:27:06 +0100

    fix provisioning script: update file path to search for .yaml.template files in the correct directory

--- grafana/provisioning/generate-provisioning.sh
+++ grafana/provisioning/generate-provisioning.sh
@@ -3,13 +3,13 @@ set -e
 
 echo "Replacing environment variables in provisioning files..."
 
-# Find all files with .yaml.template extension within /workspace/provisioning and its subdirectories
-find /workspace/provisioning -type f -name "*.yaml.template" | while read -r file; do
-  # Calculate the relative path of the file from /workspace/provisioning
-  relative_path="${file#/workspace/provisioning/}"
+# Find all files with .yaml.template extension within /provisioning and its subdirectories
+find /provisioning -type f -name "*.yaml.template" | while read -r file; do
+  # Calculate the relative path of the file from /provisioning
+  relative_path="${file#/provisioning/}"
   
   # Construct the target file path by removing the .template extension
-  target_file="/workspace/provisioning/${relative_path%.template}"
+  target_file="/provisioning/${relative_path%.template}"
 
   # Create the target directory if it doesn't exist, preserving the subdirectory structure
   mkdir -p "$(dirname "$target_file")"
